04c49bd8d4a67de3395319f3f09bcc6e17f635a7
Pamela Rdz <pamela.rodriguezd@udem.edu>
2026-05-16 16:18:19 -0600

    Agregar Archivos Diseño Solamente, archivos kt son de mientras

--- app/src/main/java/com/estudiante/strennus_proyweb/ui/LogInActivity.kt
+++ app/src/main/java/com/estudiante/strennus_proyweb/ui/LogInActivity.kt
@@ -1,21 +1,15 @@
 package com.estudiante.strennus_proyweb.ui
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.estudiante.strennus_proyweb.R
+import android.os.Bundle
+import com.estudiante.strennus_proyweb.databinding.ActivityLoginBinding
 
 class LogInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_log_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
